2d9addfd34bfcfe549c037473c5a30b1b8089bd6
Febriyanto35 <128132189+Febriyanto35@users.noreply.github.com>
2024-06-20 18:27:10 +0800

    Add files via upload

---
+++ frontend/controllers/komen.js
@@ -0,0 +1,55 @@
+import { Comment, ForumDiscussion, Users } from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+
+export const addComment = async (req, res, next) => {
+  try {
+    const token = req.cookies.refreshToken;
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+    const userId = decodedToken.userId;
+
+    const { id: forumDiscussionId } = req.params;
+    const { comment } = req.body;
+
+    const forumDiscussion = await ForumDiscussion.findByPk(forumDiscussionId, {
+      include: [
+        {
+          model: Comment,
+          as: 'Comments',
+          attributes: ['id', 'comment'],
+          include: [
+            {
+              model: Users,
+              as: 'User',
+              attributes: ['id', 'name'],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!forumDiscussion) {
+      return res.status(404).json({ error: "Forum discussion not found" });
+    }
+
+    const user = await Users.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const newComment = await Comment.create({
+      forumDiscussionId,
+      userId,
+      name: user.name,
+      comment,
+    });
+
+    res.status(201).json({ comment: newComment, comments: forumDiscussion.Comments });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
---
+++ frontend/routes/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { Home, Login, Register, Logout, getUserProfile, updateProfile } from "../controllers/Users.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { refreshToken } from "../controllers/Refreshtoken.js";
+import multer from 'multer';
+import { getAllHerbs, searchHerbs } from '../controllers/allHerbs.js';
+import { getForumDiscussion, createForum, deleteForum } from '../controllers/forum.js';
+import { addComment } from '../controllers/komen.js';
+
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.get('/', Home)
+router.post('/register', Register);
+router.post('/login', Login);
+router.get('/profile', verifyToken, getUserProfile);
+router.post('/profile', verifyToken, upload.single('file'), updateProfile);
+router.get('/search', searchHerbs);
+router.get('/allHerbs', getAllHerbs);
+router.get('/forum-discussion', getForumDiscussion);
+router.post('/forum-discussion', upload.single('photoDiscussionUrl'), verifyToken, createForum);
+router.post('/forum-discussion/:id/comment', verifyToken, addComment);
+router.delete('/forum-discussion/:id', deleteForum);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+
+export default router;
